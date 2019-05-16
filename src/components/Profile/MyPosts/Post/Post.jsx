91cc7154e6f710d906b055d2b7293cfe75aa28aa
unknown <savva.antonyuk.spb@gmail.com>
2019-05-17 02:34:35 +0300

    props

---
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import s from './Post.module.css'
+
+const Post = (props) => {
+    return (
+                    <div className={s.item}>
+                        <img src='https://images.newscientist.com/wp-content/uploads/2019/04/08111018/screenshot-2019-04-08-10.24.34.jpg' />
+                        {props.message}
+                        <div>
+                            <span>{props.likes} likes</span>
+                        </div>
+                    </div>
+                    
+    );
+}
+
+export default Post;
---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import s from './Profile.module.css'
+import MyPosts from'./MyPosts/MyPosts.jsx';
+
+const Profile = () => {
+    return (
+        <div className={s.content}>
+            <div className={s.cover}>
+                <img src='https://images.pexels.com/photos/459225/pexels-photo-459225.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940' />
+            </div>
+            <div className={s.ava}>
+                <img src='https://www.w3schools.com/w3css/img_lights.jpg' />
+            </div>
+            <div>
+                <MyPosts />
+            </div>
+        </div>
+    );
+}
+
+export default Profile;
